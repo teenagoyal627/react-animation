9dfd395f7184249cfb8848b88fd76d6fa2803b5a
teenagoyal627 <teenagoyal627@gmail.com>
2024-11-20 16:40:33 +0530

    react-animation project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+// import {motion} from 'framer-motion'
+// const App=()=>{
+//   const[x,setX]=useState(0)
+//   const[y,setY]=useState(0)
+//   const[rotate,setRotate]=useState(0)
+
+//   return (
+//     <div id="demo">
+//       <motion.div id="box" animate={{x:x,y:y,rotate:rotate}} transition={
+//         {duration:0.3,
+//         type:'spring'}
+//       }/>
+//         <div id="inputs">
+//           <p>
+//             <label htmlFor="x">X</label>
+//             <input type="number" id="x" onChange={(event)=>setX(+event.target.value)} />
+//           </p>
+         
+//           <p>
+//             <label htmlFor="y">Y</label>
+//             <input type="number" id="y" onChange={(event)=>setY(+event.target.value)} />
+//           </p>
+          
+//           <p>
+//             <label htmlFor="rotate">Rotate</label>
+//             <input type="number" id="rotate" onChange={(event)=>setRotate(+event.target.value)} />
+//           </p>
+//           </div>      
+
+//     </div>
+//   )
+// }
+// export default App;
+
+
+
+
+import WelcomePage from './pages/Welcome.jsx';
+import ChallengesPage from './pages/Challenges.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <WelcomePage /> },
+  { path: '/challenges', element: <ChallengesPage /> },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
